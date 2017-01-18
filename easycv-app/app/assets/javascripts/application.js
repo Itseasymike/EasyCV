6450b2720e1fe852c59dee79d588f578daa8f6e3
Michael Rivera <mrivera.dev@gmail.com>
2017-01-18 16:19:34 -0500

    game testing

--- easycv-app/app/assets/javascripts/application.js
+++ easycv-app/app/assets/javascripts/application.js
@@ -19,8 +19,36 @@
 
       $('.parallax').parallax();
 
-
       $('.carousel.carousel-slider').carousel({full_width: true});
+
+
+    //Functionality for Game
+    console.log("Loading scripts");
+
+     var resume = $('<img src="assets/evil-resume.png" alt="resume" class="resume">');
+     var goHome = $('<div id="home"><p>Hey, playing games is great and all but how about we create that one awesome resume you always wanted?</p><a href="/resumes">Build my resume!</a><br /><a id="exit" href="#">No, I\'\m not ready yet!</a></div>');
+     //var goHome = $('<div id="modal1" class="modal"><div class="modal-content"><h4>Modal Header</h4><p>I think it may be time to put the game down and focus on creating that awesome resume!</p></div><div class="modal-footer"><a href="/resumes" class=" modal-action modal-close waves-effect waves-green btn-flat">Build your resume!</a><a href="#!" class="modal-action modal-close waves-effect waves-green btn-flat" id="close">No Im not ready yet!</a></div></div>');
+
+      function showImage() {
+        var popImg = setInterval(function() {
+          $(' #game_container ').append(resume)
+        },1000);
+        var gameOver = setInterval(function() {
+          $(' #game_container ').append(goHome);
+        }, 10000);
+      }
+      showImage();
+
+
+      $(document).on('click', '.resume', function(e) {
+        $(this).remove();
+      });
+
+      resume.css('right', Math.floor(Math.random()*(1200-320) + 320));
+
+      $(' #exit ').click(function(e) {
+        location.reload();
+      });
 
 
 });
